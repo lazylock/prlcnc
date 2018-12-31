105e0e832644269fb59ce645bb07beca1d776abe
lazylock <sendwinston@gmail.com>
2018-12-30 22:51:48 -0800

    deploy

---
+++ precache-manifest.33950a29b7f58091d5b1b4c5afebb1ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/prlcnc/robots.txt"
+  },
+  {
+    "revision": "6af15a5e8a236a158785",
+    "url": "/prlcnc/js/chunk-vendors.1b1fc091.js"
+  },
+  {
+    "revision": "8bc7e7d583d4c5857ccd",
+    "url": "/prlcnc/js/app.f88b15d1.js"
+  },
+  {
+    "revision": "8c39e68769814800164927de4348ccff",
+    "url": "/prlcnc/index.html"
+  },
+  {
+    "revision": "a870fa92463e00876d49f0f5a9233417",
+    "url": "/prlcnc/data.xlsx"
+  },
+  {
+    "revision": "8bc7e7d583d4c5857ccd",
+    "url": "/prlcnc/css/app.cc963a15.css"
+  },
+  {
+    "revision": "c99827df788b7f32f3ad4d67f90a9b60",
+    "url": "/prlcnc/browserconfig.xml"
+  },
+  {
+    "revision": "2fbf034d126fde68709f1469c70a690d",
+    "url": "/prlcnc/apple-touch-icon.png"
+  }
+];
